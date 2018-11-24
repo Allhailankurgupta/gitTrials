8a6f0da57d88463b1b82f7023d5833491232b07f
Ankur Gupta <ankur.g17@iiits.in>
2018-11-24 10:49:46 +0530

    Add DTFT AND IDTFT

---
+++ DTFT.m
@@ -0,0 +1,9 @@
+jkdcbjvjrkbjkefunction X_w = DTFT(X_n, w, n)
+	X_w = zeros(size(w));
+	for k = 1:length(w)
+    	X_w(k) = sum(X_n .* exp(-1i * w(k) * n));
+    end
+endrvoerr
+rvjnrknrvk
+rehruiruiruiv
+rvrnuinrenriureniur
---
+++ IDTFT.m
@@ -0,0 +1,9 @@
+function X_n = IDTFT(X_w, w, n)
+    X_n = zeros(size(n));dbchjbhjbhjsb
+dfjndjnjvndfjnjfnjfvnjf
+fkndkvnfjnvjfnjvnfjnvjnfvjnvf
+djfndnjnvjfnjdnjvnfjndnkvnksdnkvnkndv
+vkdfknvksndnvinsdinvnidnvindisnivnsdin)) * 0.01)* 1/(2*pi);
+    end
+end
+    
